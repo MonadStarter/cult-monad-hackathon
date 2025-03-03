63317897becd2adc5395d863da3113bd0b73eab0
moandad <moandad202@gmail.com>
2025-03-03 01:43:56 -0700

    Added airdrop manual feature

--- packages/hardhat/scripts/createMerkleProofs.ts
+++ packages/hardhat/scripts/createMerkleProofs.ts
@@ -1,19 +1,14 @@
 const { MerkleTree } = require("merkletreejs");
 const { ethers } = require("ethers");
 
-const addresses = [
-  "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2",
-  "0x7909bC836c98bE432c43CF58CE9442a6564026aE",
-];
+const addresses = ["0x60187Bc4949eE2F01b507a9F77ad615093f44260", "0x4b6fff85ec612a536eb34e97a233ad0b82881dab"];
 
 async function generateMerkleTreeAndProofs(addresses: string[]) {
   const totalAmountInBasisPoints = 1000000; // 100% = 1,000,000 basis points
   const percentagePerUser = totalAmountInBasisPoints / addresses.length;
 
-  const leaves = addresses.map((address) =>
-    ethers.keccak256(
-      ethers.solidityPacked(["address", "uint256"], [address, percentagePerUser])
-    )
+  const leaves = addresses.map(address =>
+    ethers.keccak256(ethers.solidityPacked(["address", "uint256"], [address, percentagePerUser])),
   );
 
   const tree = new MerkleTree(leaves, ethers.keccak256, {
@@ -24,7 +19,6 @@ async function generateMerkleTreeAndProofs(addresses: string[]) {
   const proofs = addresses.map((_, index) => tree.getHexProof(leaves[index]));
   console.log("Merkle Root:", root);
   console.log("Proof for first address:", proofs[0]);
-
 
   return { root, proofs };
 }
--- packages/nextjs/components/coin/AirdropSection.tsx
+++ packages/nextjs/components/coin/AirdropSection.tsx
@@ -1,14 +1,18 @@
 import ProgressBar from "../common/ProgressBar";
-import { useContractReads } from "wagmi";
+import { formatEther, parseEther } from "viem";
+import { useContractReads, useWriteContract } from "wagmi";
 import { AirdropContractABI } from "~~/constants/abis";
 import { MERKLE_PROOFS, TEST_MERKE_ROOT } from "~~/constants/merkleRoots";
+import { useTransactor } from "~~/hooks/scaffold-eth/useTransactor";
 //import { UserIcon } from "~~/icons/symbols";
 import { useTokenStore } from "~~/stores/tokenStore";
 
 //TODO: this should be from contract or store in event
 function AirdropSection() {
-  const { userAddress } = useTokenStore();
-  const airdropContractaddress = "0xDE93ec6b61A285D0DF219C8daD8D1D2Fb5C5a4C7";
+  const { userAddress, subgraphData } = useTokenStore();
+  const airdropContractaddress = "0x44143C32EE5921c37ddE78F68648685EbC834Fd1";
+  const { writeContractAsync, isPending } = useWriteContract();
+  const writeTx = useTransactor();
   const { data, isError, isLoading } = useContractReads({
     contracts: [
       {
@@ -24,13 +28,33 @@ function AirdropSection() {
       },
     ],
   });
-  console.log("airdrop data", data);
+  const writeBuyAsyncWithParams = async () => {
+    try {
+      await writeTx(() =>
+        writeContractAsync({
+          address: airdropContractaddress,
+          abi: AirdropContractABI,
+          functionName: "claim",
+          args: [500000, [MERKLE_PROOFS[0].merkleProofs[0]]],
+        }),
+      );
+    } catch (e: any) {
+      console.log(e.message || "Purchase failed");
+    }
+  };
+
   if (isError) {
     console.log("error", isError);
   }
   if (isLoading) {
     console.log("loading", isLoading);
   }
+
+  const claimAmount = data?.[0].result ? formatEther(data?.[0].result.toString()) : 0;
+  const canClaim = data?.[1].result ? data?.[1].result : false;
+
+  console.log("claimAmount", subgraphData);
+  console.log("canClaim", canClaim);
 
   return (
     <div className="content-wrapper-card p-5 flex flex-col gap-5">
@@ -45,10 +69,31 @@ function AirdropSection() {
       {/* <p className="text-white-76 text-sm">
         You can claim {subgraphData?.cultToken?.symbol} still available for sale in the bonding curve.
       </p> */}
-      <p className="text-white-76 text-sm">
-        When the market cap reaches $ 105,548.4 all the liquidity from the bonding curve will be deposited into SunSwap
-        and burned. Progression increases as the price goes up.
-      </p>
+      {canClaim ? (
+        <p className="text-white-76 text-sm">
+          You can claim {claimAmount} {subgraphData?.cultToken?.symbol} allocation for your airdrop
+        </p>
+      ) : (
+        <p className="text-white-76 text-sm">
+          You do not have any allocation, diamond hand tokens to increase chances of next airdrop
+        </p>
+      )}
+
+      {canClaim ? (
+        <button
+          onClick={() => writeBuyAsyncWithParams()}
+          className="bg-primary-500 w-full mt-2 justify-center px-4 py-2 rounded-lg text-white hover:bg-primary-600 disabled:bg-gray-600 disabled:cursor-not-allowed"
+        >
+          Claim
+        </button>
+      ) : (
+        <button
+          onClick={() => console.log("show popup to diamond hand user owned tokens to increase probability")}
+          className="bg-primary-500 w-full mt-2 justify-center px-4 py-2 rounded-lg text-white hover:bg-primary-600 disabled:bg-gray-600 disabled:cursor-not-allowed"
+        >
+          Diamond Hand
+        </button>
+      )}
     </div>
   );
 }
